00a8491584af673146249d5f879cf2a0503f4e65
maksbotan <kolmax94@gmail.com>
2011-03-17 13:07:39 -0700

    Edited apps/test.asm via GitHub

--- apps/test.asm
+++ apps/test.asm
@@ -2,9 +2,9 @@ import:
 	include "libstd.inc"
 	std::putc, std::puts, std::gets
 data:
-	string	biff	fill(20)
+	string	buf	fill(20)
 code:
-	call puts( "What's ur name, bro? " )
-	call gets( biff )
-	call puts( "So, hello, " )
-	call puts( biff )
+	call puts("What's ur name, bro? ")
+	call gets(buf)
+	call puts("So, hello, ")
+	call puts(buf)
